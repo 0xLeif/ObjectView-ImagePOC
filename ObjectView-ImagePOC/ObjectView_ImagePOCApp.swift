6dee15908a563c88053ca10b4386a8ef2341438d
0xLeif <zmeriksen@icloud.com>
2020-11-07 12:17:07 -0600

    Initial Commit

---
+++ ObjectView-ImagePOC/ObjectView_ImagePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectView_ImagePOCApp.swift
+//  ObjectView-ImagePOC
+//
+//  Created by Zach Eriksen on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ObjectView_ImagePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
